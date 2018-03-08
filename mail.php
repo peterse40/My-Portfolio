718b756475820a6c484084dee6fe6955757a8bcc
peterse40 <peterse40@github.com>
2018-03-08 11:21:11 -0600

    added files

---
+++ mail.php
@@ -0,0 +1,23 @@
+<?php 
+
+$name = $_POST['name'];
+
+$phone = $_POST['phone'];
+
+$email = $_POST['email'];
+
+$message = $_POST['message'];
+
+$formcontent="From: $name \n Message: $message";
+
+$recipient = "info@epeterswebdesign.com";
+
+$subject = "Contact Form";
+
+$mailheader = "From: $email \r\n";
+
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+
+echo "Thank You!" . " -" . "<a href='http://epeterswebdesign.com/' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+
+?>
